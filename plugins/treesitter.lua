252bed289b1599e12012cb7207485036c8d4c298
Cristian Barrera Hinojosa <cristian.g.barrera@durham.ac.uk>
2022-12-19 10:32:58 +0000

    Fixed Vimtex-TS compatibility

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,4 +1,9 @@
 return {
   auto_install = true,
-  ensure_installed = { "python", "cpp", "c", "latex", "markdown" }
+  ensure_installed = { "python", "cpp", "c", "latex", "markdown" },
+  highlight = {
+    enable = true,
+--    disable = { "latex" },
+    additional_vim_regex_highlighting = { "latex", "markdown" },
+  },
 }
