de2ee25b356a22941c02c492197fed58f8364acd
Cristian Barrera Hinojosa <cristian.g.barrera@durham.ac.uk>
2022-12-30 17:30:18 +0000

    added support for vim skeleton files

--- polish.lua
+++ polish.lua
@@ -20,5 +20,24 @@ vim.g.vimtex_view_method='zathura'
 vim.g.vimtex_quickfix_mode=0
 vim.g.tex_conceal='abdgms'
 
+------------------------------------
+--- Skeleton files ---
+------------------------------------
+
+-- Bash scripts
+vim.api.nvim_create_autocmd("BufNewFile", {
+  pattern = "*.sh",
+  group = vim.api.nvim_create_augroup("create_skeletons", { clear = true }),
+  command = "0r ~/.skeletons/skeleton.sh"
+})
+
+-- Beamer presentations
+vim.api.nvim_create_autocmd("BufNewFile", {
+  pattern = "*beamer.tex",
+  group = vim.api.nvim_create_augroup("create_skeletons", { clear = true }),
+  command = "0r ~/.skeletons/skeleton-beamer.tex"
+})
+
+
 -----------------------------
 
