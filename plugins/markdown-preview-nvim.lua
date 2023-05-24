83a75bf87d5828d8ee1b32bc1cd18f1197f5ca41
Cristian Barrera-Hinojosa <cristian.barrera@uv.cl>
2023-05-24 12:10:52 -0400

    updated markdown plugins opts

--- plugins/markdown-preview-nvim.lua
+++ plugins/markdown-preview-nvim.lua
@@ -2,4 +2,7 @@ return {
   "iamcco/markdown-preview.nvim",
   build = "cd app && npm install",
   ft = "markdown",
+  init = function()
+    vim.g.mkdp_auto_start = 0 -- open preview window on entering buffer.
+  end,
 }
