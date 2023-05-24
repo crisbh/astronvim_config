0accda071d2c5dbc0b8c71c3c7767610a3b8ae1f
Cristian Barrera-Hinojosa <cristian.g.barrera@durham.ac.uk>
2023-05-24 12:39:18 -0400

    updated markdown opts

--- plugins/markdown-preview-nvim.lua
+++ plugins/markdown-preview-nvim.lua
@@ -3,6 +3,7 @@ return {
   build = "cd app && npm install",
   ft = "markdown",
   init = function()
-    vim.g.mkdp_auto_start = 0 -- open preview window on entering buffer.
+    vim.g.mkdp_auto_start = 0   -- open preview window on entering buffer.
+    vim.g.mkdp_refresh_slow = 1 --update preview when exiting insert mode.
   end,
 }
